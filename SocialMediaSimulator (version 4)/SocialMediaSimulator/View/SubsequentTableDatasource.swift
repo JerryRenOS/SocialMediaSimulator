362284843e0ded04f9b8eddb144f9b83ed7c99b4
JerExplosion <37779874+JerExplosion@users.noreply.github.com>
2020-03-29 11:59:52 -0400

    Add files via upload
    popularityRating

---
+++ SocialMediaSimulator (version 4)/SocialMediaSimulator/View/SubsequentTableDatasource.swift
@@ -0,0 +1,32 @@
+//
+//  SubsequentTableDatasource.swift
+//  SocialMediaSimulator
+//
+//  Created by Jerry Ren on 3/26/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import UIKit
+import Foundation
+   
+extension SubsequentViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = subsequentTable.dequeueReusableCell(withIdentifier: Constants.subsequentCellIdentity, for: indexPath)
+        
+        cell.textLabel?.text = "var Amygdala"
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ SocialMediaSimulator (version 4)/SocialMediaSimulator/ViewControllers/SubsequentViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SubsequentViewController.swift
+//  SocialMediaSimulator
+//
+//  Created by Jerry Ren on 3/25/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import UIKit
+
+class SubsequentViewController: UIViewController {
+    @IBOutlet weak var subsequentTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        subsequentTable.dataSource = self
+
+    }
+    
+    
+    
+    
+
+}
